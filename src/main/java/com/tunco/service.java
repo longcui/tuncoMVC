9af015d2ac64416db79e5fe8bf4f7a013688108c
long <dragonworld1988@gmail.com>
2015-01-24 23:00:29 +0100

    service

--- src/main/java/com/tunco/service.java
+++
@@ -1,7 +0,0 @@
-package com.tunco;
-
-/**
- * Created by dragon on 24-Jan-15.
- */
-public class service {
-}
---
+++ src/main/java/com/tunco/service/UserService.java
@@ -0,0 +1,8 @@
+package com.tunco.service;
+
+/**
+ * Created by dragon on 24-Jan-15.
+ */
+public class UserService {
+
+}
